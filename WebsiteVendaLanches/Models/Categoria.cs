495d664d25067537d86726d9503ed043fa5ae7e9
Elder-Ferreira <ferreira3.elder@gmail.com>
2024-08-21 19:02:35 -0300

    Inserido DataAnnotations na classe Categoria.cs

--- WebsiteVendaLanches/Models/Categoria.cs
+++ WebsiteVendaLanches/Models/Categoria.cs
@@ -1,14 +1,26 @@
-﻿namespace WebsiteVendaLanches.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebsiteVendaLanches.Models
 {
+    [Table("Categorias")]
     public class Categoria
     {
-     
-            public int CategoriaId { get; set; }
-            public string CategoriaNome { get; set; }
-            public string Descricao { get; set; }
+        [Key]
+        public int CategoriaId { get; set; }
+
+        [StringLength(100,ErrorMessage = "O tamanho máximo é 100 caracteres")]
+        [Required(ErrorMessage ="Informe o nome da categoria")]
+        [Display(Name ="Nome")]
+        public string CategoriaNome { get; set; }
+
+        [StringLength(200, ErrorMessage = "O tamanho máximo é 200 caracteres")]
+        [Required(ErrorMessage = "Informe a descrição da categoria")]
+        [Display(Name = "Nome")]
+        public string Descricao { get; set; }
 
 
-            public List<Lanche> Lanches { get; set; }
+        public List<Lanche> Lanches { get; set; }
     }
 }
 
