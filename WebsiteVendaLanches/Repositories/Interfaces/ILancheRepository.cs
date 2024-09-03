07fe0313fabd0f87b6107e315c8edc6209403193
Elder-Ferreira <ferreira3.elder@gmail.com>
2024-09-03 18:36:59 -0300

    Criacao da Interface ILancheRepository

---
+++ WebsiteVendaLanches/Repositories/Interfaces/ILancheRepository.cs
@@ -0,0 +1,11 @@
+﻿using WebsiteVendaLanches.Models;
+
+namespace WebsiteVendaLanches.Repositories.Interfaces
+{
+    public interface ILancheRepository
+    {
+        IEnumerable<Lanche> Lanches { get; }
+        IEnumerable<Lanche> LanchesPreferidos { get; }
+        Lanche GetLancheById(int lancheId);
+    }
+}
