75bbdbb3e47824a207a099065bdde9664555d442
Elder-Ferreira <ferreira3.elder@gmail.com>
2024-09-12 16:10:30 -0300

    Atualizada View LancheList

--- WebsiteVendaLanches/obj/Debug/net6.0/WebsiteVendaLanches.AssemblyInfo.cs
+++ WebsiteVendaLanches/obj/Debug/net6.0/WebsiteVendaLanches.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebsiteVendaLanches")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8d45ebd8a4bafe08abe4925855fee5c84e9e5c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5558770a7c428314ec7b31746453cb7df4cc294f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebsiteVendaLanches")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebsiteVendaLanches")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
