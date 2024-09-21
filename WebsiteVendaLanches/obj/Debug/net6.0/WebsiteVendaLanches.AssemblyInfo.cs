48dd8ed01b7650af0c6010f8fb59e6517d766226
Elder-Ferreira <ferreira3.elder@gmail.com>
2024-09-21 12:19:04 -0300

    Incluido metodo AdicionarAoCarrinho na classe CarrinhoCompra

--- WebsiteVendaLanches/obj/Debug/net6.0/WebsiteVendaLanches.AssemblyInfo.cs
+++ WebsiteVendaLanches/obj/Debug/net6.0/WebsiteVendaLanches.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebsiteVendaLanches")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12d97fe315dc6f53fd5703b393fd56de91a5af3c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+090056ab0e1acff319d17e672a499a27c66ec190")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebsiteVendaLanches")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebsiteVendaLanches")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
